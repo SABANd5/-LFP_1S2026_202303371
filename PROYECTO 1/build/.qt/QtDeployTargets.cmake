da6d92865c2bd33fb716edef7082ead19d9a7391
SABANd5 <cristian5sequen@gmail.com>
2026-04-02 15:22:13 -0600

    Eliminar carpeta build rastreada por accidente

--- PROYECTO 1/build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_MedLexer_FILE C:/Users/SabanD/Desktop/USAC/SEMESTRE 4/LAB LENGUAJES FORMALES/PROYECTOS/PROYECTO 1/build/MedLexer.exe)
-set(__QT_DEPLOY_TARGET_MedLexer_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_MedLexer_RUNTIME_DLLS C:/Qt/6.11.0/llvm-mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.11.0/llvm-mingw_64/bin/Qt6Gui.dll;C:/Qt/6.11.0/llvm-mingw_64/bin/Qt6Core.dll)
